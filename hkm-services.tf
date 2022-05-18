18b91dd564215adc824407a6046bdfba143c430e
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-05-18 16:52:58 +0200

    Enabling app engine API

--- hkm-services.tf
+++ hkm-services.tf
@@ -3,3 +3,9 @@ resource "google_project_service" "enable_firestore" {
     service = "firestore.googleapis.com"
     disable_dependent_services = true
 }
+
+resource "google_project_service" "enable_app_engine" {
+    project = google_project.pj-oil-dev.project_id
+    service = "appengine.googleapis.com"
+    disable_dependent_services = true
+}
