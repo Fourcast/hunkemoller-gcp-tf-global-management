85bf902574d21c2da9cee1f7c8a7efa9968f7b4f
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-05-19 13:17:09 +0200

    Fixing project ids for enabling services

--- hkm-services.tf
+++ hkm-services.tf
@@ -18,37 +18,37 @@ resource "google_project_service" "enable_app_engine_mgpr" {
 
 
 resource "google_project_service" "cloud_build_oil_dev" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-oil-dev.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_oil_stage" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-oil-staging.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_oil_prod" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-oil-prod.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_pipeline_dev" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-data-pipeline-dev.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_pipeline_stage" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-data-pipeline-stage.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_pipeline_prod" {
-    project = google_project.pj-global-management.project_id
+    project = google_project.pj-data-pipeline-prod.project_id
     service = "cloudbuild.googleapis.com"
     disable_dependent_services = true
 }
