1e67f13672a2164e4f00c269914739a9c07f113c
Stefan <stefan.neacsu@devoteam.com>
2022-05-30 11:34:10 +0200

    chore: create sa for zeenea

--- hkm-service-accounts.tf
+++ hkm-service-accounts.tf
@@ -1,42 +1,42 @@
 module "global_management_service_accs" {
-    # source = "./modules/iam/basic"
-    source = "git@github.com:hunkemollerbv/gcp-tf-modules.git//iam/basic"
+  # source = "./modules/iam/basic"
+  source = "git@github.com:hunkemollerbv/gcp-tf-modules.git//iam/basic"
 
 
-    groups              = {}
-    projects            = {}
-    folders             = {
-        # "data-team-management" = {
-        #     folder_id   = google_folder.data_team.name
-        #     groups      = null
-        #     sa          = {
-        #         "${var.data_team_mgmt_project_sa_name}" = [
-        #             "roles/bigquery.admin",
-        #             "roles/storage.objectAdmin",
-        #         ]
-        #     }
-        # }
-        # "data-team-management-cloud-composer" = {
-        #     folder_id   = google_folder.data_pipeline.name
-        #     groups      = null
-        #     sa          = {
-        #         "${var.data_team_mgmt_project_sa_name}" = [
-        #             "roles/composer.admin"
-        #         ]
-        #     }
-        # }
+  groups   = {}
+  projects = {}
+  folders  = {
+    # "data-team-management" = {
+    #     folder_id   = google_folder.data_team.name
+    #     groups      = null
+    #     sa          = {
+    #         "${var.data_team_mgmt_project_sa_name}" = [
+    #             "roles/bigquery.admin",
+    #             "roles/storage.objectAdmin",
+    #         ]
+    #     }
+    # }
+    # "data-team-management-cloud-composer" = {
+    #     folder_id   = google_folder.data_pipeline.name
+    #     groups      = null
+    #     sa          = {
+    #         "${var.data_team_mgmt_project_sa_name}" = [
+    #             "roles/composer.admin"
+    #         ]
+    #     }
+    # }
 
-        # "network-management" = {
-        #     folder_id = google_folder.networking.name
-        #     groups    = null
-        #     sa        = {
-        #         "${var.network_team_mgmt_project_sa_name}" = [
-        #             "roles/compute.xpnAdmin",
-        #             "roles/compute.networkAdmin",
-        #             "roles/compute.securityAdmin"
-        #         ]
-        #     }
-        # }
+    # "network-management" = {
+    #     folder_id = google_folder.networking.name
+    #     groups    = null
+    #     sa        = {
+    #         "${var.network_team_mgmt_project_sa_name}" = [
+    #             "roles/compute.xpnAdmin",
+    #             "roles/compute.networkAdmin",
+    #             "roles/compute.securityAdmin"
+    #         ]
+    #     }
+    # }
 
     #     "sandbox-management" = {
     #         folder_id = google_folder.sandboxes.name
@@ -47,24 +47,30 @@ module "global_management_service_accs" {
     #             ]
     #         }
     #     }
+  }
+
+  service_accounts = {
+    # Manually created service accounts
+    # SA for creating signed URLs
+    "jesper-signurl-sa" = {
+      email  = "jesper-signurl-sa@pj-hkm-global-management.iam.gserviceaccount.com"
+      create = false
     }
 
-    service_accounts = {
-        # Manually created service accounts
-        # SA for creating signed URLs
-        "jesper-signurl-sa" = {
-            email   = "jesper-signurl-sa@pj-hkm-global-management.iam.gserviceaccount.com"
-            create  = false
-        }
+    # Terraform cloud SA
+    "hkm-tfcloud-sa" = {
+      email  = "hkm-tfcloud-sa@pj-hkm-global-management.iam.gserviceaccount.com"
+      create = false
+    }
 
-        # Terraform cloud SA
-        "hkm-tfcloud-sa" = {
-            email   = "hkm-tfcloud-sa@pj-hkm-global-management.iam.gserviceaccount.com"
-            create  = false
-        }
+    # Zeenea SA
+    "hkm-zeenea-sa" = {
+      email  = "hkm-zeenea-sa@pj-hkm-global-management.iam.gserviceaccount.com"
+      create = false
+    }
 
-        # Service accounts to create via terraform
-        # Data Team Management SA
+    # Service accounts to create via terraform
+    # Data Team Management SA
     #     "${var.data_team_mgmt_project_sa_name}" = {
     #         servacc_name   = var.data_team_mgmt_project_sa_name
     #         gcp_project_id = var.data_team_mgmt_project_id
@@ -87,5 +93,5 @@ module "global_management_service_accs" {
     #         description    = "Service account for networking management"
     #         display_name   = "Network Management SA"
     #     }
-    }
+  }
 }
